202ef55807ee81b4698c75ce5ac7ec9abfa88136
Lucas-Belucci-Bellini <2.1430682e+08+Lucas-Belucci-Bellini@users.noreply.github.com>
2026-08-20 05:59:50 +0000

    refactor(militar): promote typed curation consumer

--- src/pages/militar.ts
+++ src/pages/militar.ts
@@ -3,8 +3,8 @@ import { router } from '../core/router.js';
 import { lineIcon } from '../utils/icons.js';
 import { buildImmersiveHero } from '../utils/immersive.js';
 import { fetchWikiSummary, wikiArticleUrl } from '../utils/wikipedia.js';
-import { fetchMilCuration } from '../utils/mil-curation.js';
-import type { MilCurationMap } from '../utils/mil-curation.js';
+import { fetchMilCuration } from '../utils/mil-curation';
+import type { MilCurationMap } from '../utils/mil-curation';
 import type { WikiSummary } from '../utils/wikipedia.js';
 import '../styles/centro-militar.css';
 
